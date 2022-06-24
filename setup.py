b7d17b03f191bb54486578db7c10a487e800f1d3
Ariel Rokem <arokem@gmail.com>
2022-06-24 23:09:52 +0100

    More installation/configuration infrastructure.
    Including automatic setting of the version string from git.

--- setup.py
+++ setup.py
@@ -1,25 +1,6 @@
 import glob
-import os
-
-from setuptools import setup, find_packages
-
-TRX_MINIMAL_INSTALL = os.environ.get('TRX_MINIMAL_INSTALL')
-TRX_MINIMAL_INSTALL = False if TRX_MINIMAL_INSTALL is None else \
-    int(TRX_MINIMAL_INSTALL)
-
-if TRX_MINIMAL_INSTALL:
-    SCRIPTS = None
-    SCRIPTS = glob.glob("scripts/tff_generate_trx_from_scratch.py")
-    REQUIRES = ['numpy>=1.20.*', 'nibabel>=3.*',
-                'pytest>=7.*', 'pytest-console-scripts>=0.*']
-else:
-    SCRIPTS = glob.glob("scripts/*.py")
-    REQUIRES = ['fury@git+https://github.com/frheault/fury.git@5059a529#egg=fury',
-                'dipy@git+https://github.com/frheault/dipy.git@4e192c5c6#egg=dipy',
-                'pytest>=7.*', 'pytest-console-scripts>=0.*']
+from setuptools import setup
 
 setup(name='trx',
-      packages=find_packages(),
-      setup_requires=['packaging>=19.0'],
-      install_requires=REQUIRES,
-      scripts=SCRIPTS)
+      use_scm_version=True,
+      scripts=glob.glob("scripts/*.py"))
