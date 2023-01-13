3aee2aad316980c6eadca5c53919440808d5bf6b
Ariel Rokem <arokem@gmail.com>
2023-01-12 20:08:10 -0800

    Install setuptools_scm as a requirement for setup.
    Let's see if we can't get the right version going...

--- setup.py
+++ setup.py
@@ -22,10 +22,11 @@ def local_version(version):
         return ""
 
 
-opts = dict(use_scm_version={"root": ".", "relative_to": __file__,
-                     "write_to": op.join("trx", "version.py"),
-                     "local_scheme": local_version},
-            scripts=glob.glob("scripts/*.py"))
+opts = dict(use_scm_version={
+    "root": ".", "relative_to": __file__,
+    "write_to": op.join("trx", "version.py"),
+    "local_scheme": local_version},
+    scripts=glob.glob("scripts/*.py"))
 
 
 if __name__ == '__main__':
