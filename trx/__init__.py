baf2ebeee17c7e2da96d8927dae4b13b5b69bdcd
Ariel Rokem <arokem@gmail.com>
2026-02-04 10:54:33 -0800

    Simplify by a lot. Don't need to cover some exotic cases.

--- trx/__init__.py
+++ trx/__init__.py
@@ -3,12 +3,4 @@
 try:
     from ._version import __version__  # noqa: F401
 except ImportError:
-    try:
-        from importlib.metadata import PackageNotFoundError, version
-    except ImportError:  # pragma: no cover
-        __version__ = "0+unknown"
-    else:
-        try:
-            __version__ = version("trx-python")
-        except PackageNotFoundError:
-            __version__ = "0+unknown"
+    __version__ = "unknown"
