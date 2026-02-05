6b4a53dd3838fd4ba5dfaa49626083cb16d088d5
Ariel Rokem <arokem@gmail.com>
2026-02-05 06:22:40 -0800

    Import a __version__ attribute into top-level namespace (#102)
    NF: Import a __version__ attribute into top-level namespace

--- trx/__init__.py
+++ trx/__init__.py
@@ -1,1 +1,11 @@
 """TRX file format for brain tractography data."""
+
+try:
+    from ._version import __version__  # noqa: F401
+except ImportError:
+    try:
+        from importlib.metadata import PackageNotFoundError, version
+
+        __version__ = version("trx-python")
+    except (ImportError, PackageNotFoundError):
+        __version__ = "unknown"
