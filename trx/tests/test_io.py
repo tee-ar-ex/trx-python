25cc1099b70c4d69ef83fa433be78cce248ed957
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2026-08-08 09:47:00 +0200

    STY: Prefer using `pytest` temporary path fixtures in tests
    Prefer using the `pytest` `tmp_path` fixture in tests over the standard
    library `tempfile.TemporaryDirectory()`:
    - Makes the code more readable as no additional context (and the
      associated indentation) required by the use of `TemporaryDirectory` is
      needed.
    - Simplifies path manipulation as they return a `pathlib.Path` object
      and thus, no recurrent casting to `Path` is necessary.
    - Increases consistency in temporary directory naming, and thus, eases
      maintenance.
    - Fixes
      ```
      Shadows name 'tmp_dir' from outer scope
      ```
      warnings across `test_memmap.py`.
    - Remove the unused/unnecessary `tmp_dir = get_trx_tmp_dir()` statement.

--- trx/tests/test_io.py
+++ trx/tests/test_io.py
@@ -2,7 +2,6 @@
 
 from copy import deepcopy
 import os
-from tempfile import TemporaryDirectory
 import zipfile
 
 import numpy as np
@@ -27,30 +26,28 @@ fetch_data(get_testing_files_dict(), keys=["gold_standard.zip"])
 
 @pytest.mark.parametrize("path", ["gs.trk", "gs.tck", "gs.vtk"])
 @pytest.mark.skipif(not dipy_available, reason="Dipy is not installed.")
-def test_seq_ops_sft(path):
-    with TemporaryDirectory() as tmp_dir:
-        gs_dir = os.path.join(get_home(), "gold_standard")
-        path = os.path.join(tmp_dir, path)
+def test_seq_ops_sft(tmp_path, path):
+    gs_dir = os.path.join(get_home(), "gold_standard")
+    path = os.path.join(tmp_path, path)
 
-        obj = load(os.path.join(gs_dir, "gs.trx"), os.path.join(gs_dir, "gs.nii"))
-        sft_1 = obj.to_sft()
-        save_tractogram(sft_1, path)
-        obj.close()
-        save_tractogram(sft_1, os.path.join(tmp_dir, "tmp.trk"))
+    obj = load(os.path.join(gs_dir, "gs.trx"), os.path.join(gs_dir, "gs.nii"))
+    sft_1 = obj.to_sft()
+    save_tractogram(sft_1, path)
+    obj.close()
+    save_tractogram(sft_1, os.path.join(tmp_path, "tmp.trk"))
 
-        _ = load_tractogram(os.path.join(tmp_dir, "tmp.trk"), "same")
+    _ = load_tractogram(os.path.join(tmp_path, "tmp.trk"), "same")
 
 
-def test_seq_ops_trx():
-    with TemporaryDirectory() as tmp_dir:
-        gs_dir = os.path.join(get_home(), "gold_standard")
-        path = os.path.join(gs_dir, "gs.trx")
+def test_seq_ops_trx(tmp_path):
+    gs_dir = os.path.join(get_home(), "gold_standard")
+    path = os.path.join(gs_dir, "gs.trx")
 
-        trx_1 = tmm.load(path)
-        tmm.save(trx_1, os.path.join(tmp_dir, "tmp.trx"))
-        trx_1.close()
-        trx_2 = tmm.load(os.path.join(tmp_dir, "tmp.trx"))
-        trx_2.close()
+    trx_1 = tmm.load(path)
+    tmm.save(trx_1, os.path.join(tmp_path, "tmp.trx"))
+    trx_1.close()
+    trx_2 = tmm.load(os.path.join(tmp_path, "tmp.trx"))
+    trx_2.close()
 
 
 @pytest.mark.parametrize("path", ["gs.trx", "gs.trk", "gs.tck", "gs.vtk"])
@@ -93,28 +90,25 @@ def test_load_voxmm(path):
 
 @pytest.mark.parametrize("path", ["gs.trk", "gs.trx", "gs_fldr.trx"])
 @pytest.mark.skipif(not dipy_available, reason="Dipy is not installed.")
-def test_multi_load_save_rasmm(path):
-    with TemporaryDirectory() as tmp_gs_dir:
-        gs_dir = os.path.join(get_home(), "gold_standard")
-        basename, ext = os.path.splitext(path)
+def test_multi_load_save_rasmm(tmp_path, path):
+    gs_dir = os.path.join(get_home(), "gold_standard")
+    basename, ext = os.path.splitext(path)
 
-        path = os.path.join(gs_dir, path)
-        coord = np.loadtxt(
-            os.path.join(get_home(), "gold_standard", "gs_rasmm_space.txt")
-        )
+    path = os.path.join(gs_dir, path)
+    coord = np.loadtxt(os.path.join(get_home(), "gold_standard", "gs_rasmm_space.txt"))
 
-        obj = load(path, os.path.join(gs_dir, "gs.nii"))
-        for i in range(3):
-            out_path = os.path.join(tmp_gs_dir, f"{basename}_tmp{i}_{ext}")
-            save(obj, out_path)
+    obj = load(path, os.path.join(gs_dir, "gs.nii"))
+    for i in range(3):
+        out_path = os.path.join(tmp_path, f"{basename}_tmp{i}_{ext}")
+        save(obj, out_path)
 
-            if isinstance(obj, TrxFile):
-                obj.close()
-            obj = load(out_path, os.path.join(gs_dir, "gs.nii"))
-
-        assert_allclose(obj.streamlines._data, coord, rtol=1e-04, atol=1e-06)
         if isinstance(obj, TrxFile):
             obj.close()
+        obj = load(out_path, os.path.join(gs_dir, "gs.nii"))
+
+    assert_allclose(obj.streamlines._data, coord, rtol=1e-04, atol=1e-06)
+    if isinstance(obj, TrxFile):
+        obj.close()
 
 
 @pytest.mark.parametrize("path", ["gs.trx", "gs_fldr.trx"])
